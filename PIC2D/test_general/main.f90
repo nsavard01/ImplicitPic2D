1e0a8d1fa71fdc505e1e3c09b1ed2177e01826f7
nsavard01 <nicolas@d-pace.com>
2025-02-06 14:06:58 -0800

    switch branch

--- PIC2D/test_general/main.f90
+++ PIC2D/test_general/main.f90
@@ -67,10 +67,10 @@ program main
     
     rho = e_charge * n_ave
 
-    NESW_wallBoundaries(1) = 1 ! North
-    NESW_wallBoundaries(2) = 2 ! East
-    NESW_wallBoundaries(3) = 1 ! South
-    NESW_wallBoundaries(4) = 2 ! West
+    NESW_wallBoundaries(1) = 2 ! North
+    NESW_wallBoundaries(2) = 1 ! East
+    NESW_wallBoundaries(3) = 2 ! South
+    NESW_wallBoundaries(4) = 1 ! West
 
     NESW_phiValues(1) = 0.0d0
     NESW_phiValues(2) = 0.0d0
