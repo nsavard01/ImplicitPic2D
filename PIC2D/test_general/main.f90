a1cadd0835bac3d34d3440d3bf8eb077e27ba4a0
nsavard01 <nicolas@d-pace.com>
2025-02-07 09:39:38 -0800

    switch branch

--- PIC2D/test_general/main.f90
+++ PIC2D/test_general/main.f90
@@ -177,7 +177,7 @@ program main
     EField_time = 0.0d0
     mover_time = 0.0d0
     solver_time = 0.0d0
-    number_diagnostics = 10
+    number_diagnostics = 5
     call system_clock(count_rate = timingRate)
     open(41,file='NumDiag.dat', form='UNFORMATTED', access = 'stream', status = 'new')
     write(41) number_diagnostics
